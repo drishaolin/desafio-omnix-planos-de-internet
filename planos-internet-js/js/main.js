56486a1fb984456099d00ecdfc18f6f035f9e6d5
Adriana Mucciolo <drishaolin@gmail.com>
2021-10-27 17:35:52 -0300

    logic for show offers infos

--- planos-internet-js/js/main.js
+++ planos-internet-js/js/main.js
@@ -1,3 +1,16 @@
+function newElement(element, elementClass, text) {
+    const e = document.createElement(element);
+    e.className = elementClass;
+    e.textContent = text;
+    return e;
+}
 
-console.log(offers);
+for (const offer of offers.userData.offers) {
+    const div = newElement("div", "offer-item");
+    const h2 = newElement("h2", "offer-name", offer.name);
+    const p = newElement("p", "offer-price", `R$ ${offer.price}`);
+    div.appendChild(h2);
+    div.appendChild(p);
 
+    document.getElementById("offers-list").appendChild(div);
+}
